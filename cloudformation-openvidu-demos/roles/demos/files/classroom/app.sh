dfecebd385c0735a2cb98bbce2f85a64342550d4
Fede Diaz <nordri@gmail.com>
2017-08-28 16:24:56 +0200

    Removing Docker IP from demo launcher

--- cloudformation-openvidu-demos/roles/demos/files/classroom/app.sh
+++ cloudformation-openvidu-demos/roles/demos/files/classroom/app.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# get MySQL IP
-MYSQL_IP=$(docker inspect openvidu-mysql -f '{{ .NetworkSettings.IPAddress }}')
-
-exec java -Djava.security.egd=file:/dev/./urandom -Dspring.datasource.url=jdbc:mysql://${MYSQL_IP}/openvidu_sample_app -Dopenvidu.url=https://$1:8443 -Dserver.port=5001 -jar /var/www/html/classroom/classroom-demo.jar
+exec java -Djava.security.egd=file:/dev/./urandom -Dspring.datasource.url=jdbc:mysql://127.0.0.1/openvidu_sample_app -Dopenvidu.url=https://$1:8443 -Dserver.port=5001 -jar /var/www/html/classroom/classroom-demo.jar
 
