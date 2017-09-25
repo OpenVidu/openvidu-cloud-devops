46716923beb1579bade87bed454f009845c50670
Fede Diaz <nordri@gmail.com>
2017-09-25 16:37:30 +0200

    fix typo in template

--- cloudformation-openvidu/roles/openvidu/templates/download_ov.sh
+++ cloudformation-openvidu/roles/openvidu/templates/download_ov.sh
@@ -2,6 +2,6 @@
 
 OUTPUT=/opt/workdir/cloudformation-openvidu/roles/openvidu/files/openvidu-server.jar
 
-curl -L -o $OUTPUT https://github.com/OpenVidu/openvidu/releases/download/{% version %}/openvidu-server-{%version %}.jar
+curl -L -o $OUTPUT https://github.com/OpenVidu/openvidu/releases/download/{{ version }}/openvidu-server-{{ version }}.jar
 
 
