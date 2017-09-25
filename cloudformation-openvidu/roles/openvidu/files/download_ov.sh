a16715f6ca58b3aef11606528c741d368cec4158
Fede Diaz <nordri@gmail.com>
2017-09-25 16:55:11 +0200

    update template

--- cloudformation-openvidu/roles/openvidu/files/download_ov.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OUTPUT=/opt/workdir/cloudformation-openvidu/roles/openvidu/files/openvidu-server.jar
-RELEASE_URL=https://api.github.com/repos/openvidu/openvidu/releases/latest
-DOWNLOAD_URL=$(curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep openvidu-server
-)
-curl -L -o $OUTPUT $DOWNLOAD_URL
-
-
--- cloudformation-openvidu/roles/openvidu/templates/download_ov.sh
+++ cloudformation-openvidu/roles/openvidu/templates/download_ov.sh
@@ -2,6 +2,11 @@
 
 OUTPUT=/opt/workdir/cloudformation-openvidu/roles/openvidu/files/openvidu-server.jar
 
-curl -L -o $OUTPUT https://github.com/OpenVidu/openvidu/releases/download/{{ version }}/openvidu-server-{{ version }}.jar
-
+{% if version == "latest" %}
+RELEASE_URL=https://api.github.com/repos/openvidu/openvidu/releases/latest
+DOWNLOAD_URL=$(curl -s $RELEASE_URL | grep browser_download_url | cut -d '"' -f 4 | grep openvidu-server)
+curl -L -o $OUTPUT $DOWNLOAD_URL
+{% else %}
+curl -L -o $OUTPUT https://github.com/OpenVidu/openvidu/releases/download/v{{ version }}/openvidu-server-{{ version }}.jar
+{% endif %}
 
