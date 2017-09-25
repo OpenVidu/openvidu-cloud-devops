1a39b8c5f427b0775011787d9f03220dee7b6b45
Fede Diaz <nordri@gmail.com>
2017-09-25 16:23:40 +0200

    We can now use diferent versions for OV server

---
+++ cloudformation-openvidu/roles/openvidu/templates/download_ov.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+OUTPUT=/opt/workdir/cloudformation-openvidu/roles/openvidu/files/openvidu-server.jar
+
+curl -L -o $OUTPUT https://github.com/OpenVidu/openvidu/releases/download/{% version %}/openvidu-server-{%version %}.jar
+
+
