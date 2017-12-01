4d24f5b021a4c21f68c7cce976a195d3d5d09719
Fede Diaz <nordri@gmail.com>
2017-12-01 12:07:31 +0100

    shutdown proof

---
+++ cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+
+java -jar -Dopenvidu.secret="{{ openvidusecret }}" -Dserver.ssl.enabled=false -Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:8443 -Dserver.port=5443 /opt/openvidu/openvidu-server.jar
+
+
