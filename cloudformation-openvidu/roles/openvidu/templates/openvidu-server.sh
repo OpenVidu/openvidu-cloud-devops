fe5a7c83e6dedce12447b15f4eb5887233962e73
Fede Diaz <nordri@gmail.com>
2018-02-06 12:19:07 +0100

    move launch script

--- cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
+++ cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+{% if whichcert == "letsencrypt" or whichcert == "owncert" %}
+PUBLIC_HOSTNAME={{ domain_name }}
+{% else %}
 PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+{% endif %}
 
 java -jar -Dopenvidu.secret="{{ openvidusecret }}" -Dopenvidu.recording=true -Dopenvidu.recording.free-access={{ FreeHTTPAccesToRecordingVideos }} -Dserver.ssl.enabled=false -Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:8443 -Dserver.port=5443 -DMY_UID=$(id -u $USER) /opt/openvidu/openvidu-server.jar
 
