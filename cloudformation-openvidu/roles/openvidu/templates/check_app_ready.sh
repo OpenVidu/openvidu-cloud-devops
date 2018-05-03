5d3ee9155bf06c202fbac1cbdabc73a6639dc942
Fede Diaz <nordri@gmail.com>
2018-05-03 11:09:32 +0200

    Openvidu port parametrized

--- cloudformation-openvidu/roles/openvidu/templates/check_app_ready.sh
+++ cloudformation-openvidu/roles/openvidu/templates/check_app_ready.sh
@@ -4,7 +4,7 @@ sleep 1m
 
 while true
 do 
-  HTTP_STATUS=$(curl -Ik https://localhost:8443 | head -n1 | awk '{print $2}')
+  HTTP_STATUS=$(curl -Ik https://localhost:{{ openvidu_port }} | head -n1 | awk '{print $2}')
   if [ $HTTP_STATUS == 401 ]; then
     break
   fi
--- cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
+++ cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
@@ -6,5 +6,5 @@ PUBLIC_HOSTNAME={{ domain_name }}
 PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 {% endif %}
 
-java -jar -Dopenvidu.secret="{{ openvidusecret }}" -Dopenvidu.recording=true -Dopenvidu.recording.free-access={{ FreeHTTPAccesToRecordingVideos }} -Dserver.ssl.enabled=false -Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:8443 -Dserver.port=5443 -DMY_UID=$(id -u $USER) /opt/openvidu/openvidu-server.jar
+java -jar -Dopenvidu.secret="{{ openvidusecret }}" -Dopenvidu.recording=true -Dopenvidu.recording.free-access={{ FreeHTTPAccesToRecordingVideos }} -Dserver.ssl.enabled=false -Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:{{ openvidu_port }} -Dserver.port=5443 -DMY_UID=$(id -u $USER) /opt/openvidu/openvidu-server.jar
 
