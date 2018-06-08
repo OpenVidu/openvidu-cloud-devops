c2fad103bcf5dc2ba1257d3c9e6ab3f1de4d3d1a
nordri <nordri@gmail.com>
2018-06-08 17:54:47 +0200

    template in templates folder

--- cloudformation-openvidu/roles/openvidu/templates/download_ov.sh
+++ cloudformation-openvidu/roles/openvidu/templates/download_ov.sh
@@ -4,6 +4,6 @@
 # Ansible openvidu producction environment
 wget -O /opt/openvidu/openvidu-server.jar https://github.com/OpenVidu/openvidu/releases/download/v$1/openvidu-server-$1.jar
 {% else %}
-# Ansible openvidu producction environment
+# Ansible openvidu nightly environment
 wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
 {% endif %}
