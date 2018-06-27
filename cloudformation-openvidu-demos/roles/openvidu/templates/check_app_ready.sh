a7bcb0378f5a0c04581f90223662240b2394a154
nordri <nordri@gmail.com>
2018-06-27 17:05:41 +0200

    GET methods retunr 200 now

--- cloudformation-openvidu-demos/roles/openvidu/templates/check_app_ready.sh
+++ cloudformation-openvidu-demos/roles/openvidu/templates/check_app_ready.sh
@@ -5,7 +5,7 @@ sleep 1m
 while true
 do 
   HTTP_STATUS=$(curl -Ik https://localhost:{{ openvidu_port }} | head -n1 | awk '{print $2}')
-  if [ $HTTP_STATUS == 401 ]; then
+  if [ $HTTP_STATUS == 200 ]; then
     break
   fi
   sleep 1
