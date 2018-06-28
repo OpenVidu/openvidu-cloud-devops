68042df82b262301f3080e530147738b86b982df
nordri <nordri@gmail.com>
2018-06-28 12:30:32 +0200

    Openvidu browser parameter

--- cloudformation-openvidu-demos/roles/demos/files/openvidu_create_demos_structure.sh
+++ cloudformation-openvidu-demos/roles/demos/files/openvidu_create_demos_structure.sh
@@ -2,7 +2,8 @@
 set -eu -o pipefail
 
 # Vars
-DEMOS_RELEASE=$1
+OV_RELEASE=$1
+DEMOS_RELEASE=$2
 CF_RELEASE=master
 WORKDIR=$(mktemp -d --suffix .ov)
 TARGETDIR=/var/www/html
@@ -18,6 +19,7 @@ cd ..
 # Basic Videoconference (openvidu-insecure-js)
 mkdir -p $TARGETDIR/basic-videoconference
 cp -rav $WORKDIR/openvidu-tutorials/openvidu-insecure-js/web/* $TARGETDIR/basic-videoconference
+wget https://github.com/OpenVidu/openvidu/releases/download/v${OV_RELEASE}/openvidu-browser-${OV_RELEASE}.js -O $TARGETDIR/basic-videoconference/openvidu-browser-${OV_RELEASE}.js
 
 # Basic Webinar (openvidu-js-java)
 mkdir -p $TARGETDIR/basic-webinar
@@ -26,6 +28,7 @@ wget https://github.com/OpenVidu/openvidu-tutorials/releases/download/v${DEMOS_R
 # Getaroom
 mkdir -p $TARGETDIR/getaroom 
 cp -rav $WORKDIR/openvidu-tutorials/openvidu-getaroom/web/* $TARGETDIR/getaroom
+wget https://github.com/OpenVidu/openvidu/releases/download/v${OV_RELEASE}/openvidu-browser-${OV_RELEASE}.js -O $TARGETDIR/getaroom/openvidu-browser-${OV_RELEASE}.js
 
 # Openvidu Classroom
 mkdir -p $TARGETDIR/classroom
