43734fccf1c9149cb5306dea92f8fa6f6fa7a8cf
nordri <nordri@gmail.com>
2018-07-17 10:15:54 +0200

    Fix typo in syntax and white spaces between options in java -jar

--- cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
+++ cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
@@ -8,18 +8,18 @@ PUBLIC_HOSTNAME={{ domain_name }}
 PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 {% endif %}
 
-OPENVIDU_OPTIONS="-Dopenvidu.secret={{ openvidusecret }}"
-OPENVIDU_OPTIONS+="-Dopenvidu.recording=true"
-OPENVIDU_OPTIONS+="-Dopenvidu.recording.public-access={{ FreeHTTPAccesToRecordingVideos }}"
-OPENVIDU_OPTIONS+="-Dserver.ssl.enabled=false"
-OPENVIDU_OPTIONS+="-Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:{{ openvidu_port }}"
-OPENVIDU_OPTIONS+="-Dserver.port=5443"
-OPENVIDU_OPTIONS+="-DMY_UID=$(id -u $USER)"
-OPENVIDU_OPTIONS+="-DOpenviduRecordingNotification={{ OpenviduRecordingNotification }}"
-OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMax-recv-bandwidth={{ OpenviduStreamsVideoMax-recv-bandwidth }}"
-OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMin-recv-bandwidth={{ OpenviduStreamsVideoMin-recv-bandwidth }}"
-OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMax-send-bandwidth={{ OpenviduStreamsVideoMax-send-bandwidth }}"
-OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMin-send-bandwidth={{ OpenviduStreamsVideoMin-send-bandwidth }}"
+OPENVIDU_OPTIONS="-Dopenvidu.secret={{ openvidusecret }} "
+OPENVIDU_OPTIONS+="-Dopenvidu.recording=true "
+OPENVIDU_OPTIONS+="-Dopenvidu.recording.public-access={{ FreeHTTPAccesToRecordingVideos }} "
+OPENVIDU_OPTIONS+="-Dserver.ssl.enabled=false "
+OPENVIDU_OPTIONS+="-Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:{{ openvidu_port }} "
+OPENVIDU_OPTIONS+="-Dserver.port=5443 "
+OPENVIDU_OPTIONS+="-DMY_UID=$(id -u $USER) "
+OPENVIDU_OPTIONS+="-DOpenviduRecordingNotification={{ OpenviduRecordingNotification }} "
+OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMax-recv-bandwidth={{ OpenviduStreamsVideoMaxRecvBandwidth }} "
+OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMin-recv-bandwidth={{ OpenviduStreamsVideoMinRecvBandwidth }} "
+OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMax-send-bandwidth={{ OpenviduStreamsVideoMaxSendBandwidth }} "
+OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMin-send-bandwidth={{ OpenviduStreamsVideoMinSendBandwidth }} "
 
 java -jar ${OPENVIDU_OPTIONS} /opt/openvidu/openvidu-server.jar
 
