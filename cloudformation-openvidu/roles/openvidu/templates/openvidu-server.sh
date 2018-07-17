b019dd0fd223e3b7eabee525805b298c44ae4a22
nordri <nordri@gmail.com>
2018-07-17 09:37:14 +0200

    Update for bandwidth and recording notification

--- cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
+++ cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
@@ -8,5 +8,18 @@ PUBLIC_HOSTNAME={{ domain_name }}
 PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 {% endif %}
 
-java -jar -Dopenvidu.secret="{{ openvidusecret }}" -Dopenvidu.recording=true -Dopenvidu.recording.public-access={{ FreeHTTPAccesToRecordingVideos }} -Dserver.ssl.enabled=false -Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:{{ openvidu_port }} -Dserver.port=5443 -DMY_UID=$(id -u $USER) /opt/openvidu/openvidu-server.jar
+OPENVIDU_OPTIONS="-Dopenvidu.secret={{ openvidusecret }}"
+OPENVIDU_OPTIONS+="-Dopenvidu.recording=true"
+OPENVIDU_OPTIONS+="-Dopenvidu.recording.public-access={{ FreeHTTPAccesToRecordingVideos }}"
+OPENVIDU_OPTIONS+="-Dserver.ssl.enabled=false"
+OPENVIDU_OPTIONS+="-Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:{{ openvidu_port }}"
+OPENVIDU_OPTIONS+="-Dserver.port=5443"
+OPENVIDU_OPTIONS+="-DMY_UID=$(id -u $USER)"
+OPENVIDU_OPTIONS+="-DOpenviduRecordingNotification={{ OpenviduRecordingNotification }}"
+OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMax-recv-bandwidth={{ OpenviduStreamsVideoMax-recv-bandwidth }}"
+OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMin-recv-bandwidth={{ OpenviduStreamsVideoMin-recv-bandwidth }}"
+OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMax-send-bandwidth={{ OpenviduStreamsVideoMax-send-bandwidth }}"
+OPENVIDU_OPTIONS+="-DOpenviduStreamsVideoMin-send-bandwidth={{ OpenviduStreamsVideoMin-send-bandwidth }}"
+
+java -jar ${OPENVIDU_OPTIONS} /opt/openvidu/openvidu-server.jar
 
