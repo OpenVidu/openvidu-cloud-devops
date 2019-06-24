6063a559f10a455a0bb89ef9bc7cf1e28de208c3
Fede Diaz <nordri@gmail.com>
2019-06-24 10:26:43 +0200

    New if for no AWS instances

--- clustering/roles/openvidu/templates/openvidu-server.sh
+++ clustering/roles/openvidu/templates/openvidu-server.sh
@@ -5,7 +5,11 @@
 {% if whichcert == "letsencrypt" or whichcert == "owncert" %}
 PUBLIC_HOSTNAME={{ domain_name }}
 {% else %}
+{% if run_ec2 == true %}
 PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+{% else %}
+PUBLIC_HOSTNAME={{ ov_public_hostname }}
+{% endif %}
 {% endif %}
 
 OPENVIDU_OPTIONS="-Dopenvidu.secret={{ openvidusecret }} "
@@ -28,6 +32,9 @@ OPENVIDU_OPTIONS+="-Dopenvidu.pro.cluster.load.strategy={{ OpenviduClusterLoadSt
 {% if run_ec2 == true %}
 export AWS_DEFAULT_REGION={{ aws_default_region }}
 KMS_IPs=$(aws ec2 describe-instances --query 'Reservations[].Instances[].[PrivateIpAddress]' --output text --filters Name=instance-state-name,Values=running Name=tag:ov-cluster-member,Values=kms)
+{% else %}
+KMS_IPs=$(echo {{ kms_endpoint_ips }} | tr , ' ')
+{% endif %}
 KMS_ENDPOINTS=$(for IP in $KMS_IPs
 do
   echo $IP | awk '{ print "\"ws://" $1 ":8888/kurento\"" }'
@@ -35,7 +42,6 @@ done
 )
 KMS_ENDPOINTS_LINE=$(echo $KMS_ENDPOINTS | tr ' ' ,)
 OPENVIDU_OPTIONS+="-Dkms.uris=[${KMS_ENDPOINTS_LINE}] "
-{% endif %}
 
 pushd /opt/openvidu
 exec java -jar ${OPENVIDU_OPTIONS} /opt/openvidu/openvidu-server.jar
