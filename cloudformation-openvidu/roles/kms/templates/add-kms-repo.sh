338397be9694c5b7a65b9755fa4963fd09b19311
cruizba <charlesrb94@outlook.com>
2020-02-17 19:58:47 +0100

    Changes added for alpha

--- cloudformation-openvidu/roles/kms/templates/add-kms-repo.sh
+++ cloudformation-openvidu/roles/kms/templates/add-kms-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 OV_VERSION={{ openvidu_version }}
-KMS_VERSION=$(curl --silent https://oudzlg0y3m.execute-api.eu-west-1.amazonaws.com/v1/ov_kms_matrix?ov=${OV_VERSION} | jq --raw-output '.[0] | .kms' )
+KMS_VERSION=6.13.0
 echo "deb [arch=amd64] http://ubuntu.openvidu.io/${KMS_VERSION} xenial kms6" > /etc/apt/sources.list.d/kurento.list
 apt-get update
--- cloudformation-openvidu/roles/openvidu/files/download_ov.sh
+++ cloudformation-openvidu/roles/openvidu/files/download_ov.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-if [ "$1" != "nightly" ]; then
-	# Ansible openvidu production environment
-	wget -O /opt/openvidu/openvidu-server.jar https://github.com/OpenVidu/openvidu/releases/download/v$1/openvidu-server-$1.jar
-else
-	# Ansible openvidu nightly environment
-	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
-fi
+wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/alpha/openvidu-server-$1.jar
