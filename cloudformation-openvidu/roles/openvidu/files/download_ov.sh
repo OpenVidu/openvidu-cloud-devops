fd2f960a2c492322a6ced6f8e7bd4da3f3b6252c
cruizba <charlesrb94@outlook.com>
2020-02-19 19:32:16 +0100

    Directory build snapshots in download_ov.sh

--- cloudformation-openvidu/roles/openvidu/files/download_ov.sh
+++ cloudformation-openvidu/roles/openvidu/files/download_ov.sh
@@ -4,7 +4,8 @@ if [ "$1" == "nightly" ]; then
 	# Ansible openvidu nightly environment
 	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
 elif [[ "$1" == *"SNAPSHOT"* ]]; then
-	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-$1.jar
+	# Ansible openvidu snapshot
+	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/snapshots/openvidu-server-$1.jar
 else
 	# Ansible openvidu production environment
 	wget -O /opt/openvidu/openvidu-server.jar https://github.com/OpenVidu/openvidu/releases/download/v$1/openvidu-server-$1.jar
