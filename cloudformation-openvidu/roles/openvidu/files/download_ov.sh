c7be86623344974ea3ba387233e8bb82f696f8b2
cruizba <charlesrb94@outlook.com>
2020-02-19 19:14:27 +0100

    Minor fix download_ov.sh

--- cloudformation-openvidu/roles/openvidu/files/download_ov.sh
+++ cloudformation-openvidu/roles/openvidu/files/download_ov.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ "$1" != "nightly" ]; then
-	# Ansible openvidu production environment
-	wget -O /opt/openvidu/openvidu-server.jar https://github.com/OpenVidu/openvidu/releases/download/v$1/openvidu-server-$1.jar
+if [ "$1" == "nightly" ]; then
+	# Ansible openvidu nightly environment
+	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
 elif [[ "$1" == *"SNAPSHOT"* ]]; then
 	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-$1.jar
 else
-	# Ansible openvidu nightly environment
-	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
+	# Ansible openvidu production environment
+	wget -O /opt/openvidu/openvidu-server.jar https://github.com/OpenVidu/openvidu/releases/download/v$1/openvidu-server-$1.jar
 fi
