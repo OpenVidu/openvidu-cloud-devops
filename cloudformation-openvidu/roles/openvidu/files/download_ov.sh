f6882d3aca7cd48d57db969a15e0d0fa53fe73ef
cruizba <charlesrb94@outlook.com>
2020-03-03 18:04:24 +0100

    Prevent download of openvidu to stop downloading

--- cloudformation-openvidu/roles/openvidu/files/download_ov.sh
+++ cloudformation-openvidu/roles/openvidu/files/download_ov.sh
@@ -2,11 +2,11 @@
 
 if [ "$1" == "nightly" ]; then
 	# Ansible openvidu nightly environment
-	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
+	wget -O --tries=10 --read-timeout=20 /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/nightly/latest/openvidu-server-latest.jar
 elif [[ "$1" == *"SNAPSHOT"* ]]; then
 	# Ansible openvidu snapshot
-	wget -O /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/snapshots/openvidu-server-$1.jar
+	wget -O --tries=10 --read-timeout=20 /opt/openvidu/openvidu-server.jar http://builds.openvidu.io/openvidu/snapshots/openvidu-server-$1.jar
 else
 	# Ansible openvidu production environment
-	wget -O /opt/openvidu/openvidu-server.jar https://github.com/OpenVidu/openvidu/releases/download/v$1/openvidu-server-$1.jar
+	wget -O --tries=10 --read-timeout=20 /opt/openvidu/openvidu-server.jar https://github.com/OpenVidu/openvidu/releases/download/v$1/openvidu-server-$1.jar
 fi
