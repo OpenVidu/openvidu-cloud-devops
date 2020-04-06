d5484216cc0868440234b7850aa972ed768ac319
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-04-06 16:32:58 +0200

    Update nginx

---
+++ cloudformation-openvidu/docker-compose/prueba.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sed -i "s/DOMAIN_NAME/PRUEBA/" pruebas
+sed -i "s/OPENVIDU_VERSION/PRUEBA/" pruebas
+sed -i "s/WANTDEMOS/PRUEBA/" pruebas
+sed -i "s/selfsigned/PRUEBA/" pruebas
+sed -i "s/LETS_EMAIL/PRUEBA/" pruebas
+sed -i "s/MY_SECRET/PRUEBA/" pruebas
