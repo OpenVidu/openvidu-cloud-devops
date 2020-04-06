f85e60dbdeab551298dc28537db165a364c51815
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-04-06 17:21:34 +0200

    Update openvidu options

--- cloudformation-openvidu/docker-compose/prueba.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i "s/DOMAIN_NAME/PRUEBA/" pruebas
-sed -i "s/OPENVIDU_VERSION/PRUEBA/" pruebas
-sed -i "s/WANTDEMOS/PRUEBA/" pruebas
-sed -i "s/selfsigned/PRUEBA/" pruebas
-sed -i "s/LETS_EMAIL/PRUEBA/" pruebas
-sed -i "s/MY_SECRET/PRUEBA/" pruebas
